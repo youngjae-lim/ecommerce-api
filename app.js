60084a5db20051ffbcae2b4c4d5e01ecec61e6a0
Youngjae Lim <limyoungjae@gmail.com>
2022-01-23 22:51:42 -0500

    Initial server config in app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('E-commerce API')
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
