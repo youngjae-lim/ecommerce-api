c38f6aec06c69165e4715d794a5817cbcff328c0
Youngjae Lim <limyoungjae@gmail.com>
2022-01-24 10:55:38 -0500

    Created User model

---
+++ models/User.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+import validator from 'validator'
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Please provide email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide valid email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user',
+  },
+})
+
+export default mongoose.model('User', UserSchema)
