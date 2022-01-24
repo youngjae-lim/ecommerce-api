90de3c403edfe2fb6382bf654cd786dea5fd6840
Youngjae Lim <limyoungjae@gmail.com>
2022-01-24 13:25:05 -0500

    Added a logic to register a first account as an admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,11 @@ const register = async (req, res) => {
     throw new BadRequestError('Email alreasy exists')
   }
 
-  const user = await User.create({ name, email, password })
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0
+  const role = isFirstAccount ? 'admin' : 'user'
+
+  const user = await User.create({ name, email, password, role })
 
   res.status(StatusCodes.CREATED).json({ user })
 }
