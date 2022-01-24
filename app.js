8ee39e2be0ca9a396895259c9c45186950f361d0
Youngjae Lim <limyoungjae@gmail.com>
2022-01-23 23:04:15 -0500

    Connected to mongodb

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import express from 'express'
+import connectDB from './db/connect.js'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
 
 const port = process.env.PORT || 5000
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URL)
+
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}...`)
     })
