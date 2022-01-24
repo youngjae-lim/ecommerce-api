7bb5493f467c958700246ee7691380aa8054f971
Youngjae Lim <limyoungjae@gmail.com>
2022-01-24 12:36:12 -0500

    Added initial config for register controller as well as custom error classes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,22 @@
 import User from '../models/User.js'
+import { StatusCodes } from 'http-status-codes'
+import { BadRequestError, UnauthenticatedError } from '../errors/index.js'
 
 const register = async (req, res) => {
-  res.send('register')
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    throw new BadRequestError('please provide all values')
+  }
+
+  const emailAlreadyExists = await User.findOne({ email })
+  if (emailAlreadyExists) {
+    throw new BadRequestError('Email alreasy exists')
+  }
+
+  const user = await User.create({ name, email, password })
+
+  res.status(StatusCodes.CREATED).json({ user })
 }
 
 const login = async (req, res) => {
