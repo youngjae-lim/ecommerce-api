30152f72ed084c37553e83e82dfb5906234c5152
Youngjae Lim <limyoungjae@gmail.com>
2022-01-25 11:52:55 -0500

    Initial implementation of getAllUsers & getSingleUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,11 +9,16 @@ import {
 import { attachCookiesToResponse } from '../utils/index.js'
 
 const getAllUsers = async (req, res) => {
-  res.send('get all users')
+  const users = await User.find({ role: 'user' }).select('-password')
+  res.status(StatusCodes.OK).json({ users })
 }
 
 const getSingleUser = async (req, res) => {
-  res.send('get single user')
+  const user = await User.findOne({ _id: req.params.id }).select('-password')
+  if (!user) {
+    throw new NotFoundError(`No user with id: ${req.params.id}`)
+  }
+  res.status(StatusCodes.OK).json({ user })
 }
 
 const showCurrentUser = async (req, res) => {
