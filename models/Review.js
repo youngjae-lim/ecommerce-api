033fa1ef6c721826be83c7bf5df3229a8d481cda
Youngjae Lim <limyoungjae@gmail.com>
2022-01-27 10:29:43 -0500

    Completed aggregate function to calculate avg rating and number of reviews per product

--- models/Review.js
+++ models/Review.js
@@ -35,4 +35,41 @@ const ReviewSchema = mongoose.Schema(
 // each user is allowed to leave a comment per product
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true })
 
+// static method of calculating avg rating for a product
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+  // result will be an array of object - i.e., [{_id: , averageRating: , numOfReviews: }]
+  const result = await this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: null, // ignore _id because the productId is the same to all reviews for a single product
+        averageRating: { $avg: '$rating' },
+        numOfReviews: { $sum: 1 },
+      },
+    },
+  ])
+
+  try {
+    await this.model('Product').findOneAndUpdate(
+      { _id: productId },
+      {
+        averageRating: Math.ceil(result[0]?.averageRating || 0),
+        numOfReviews: result[0]?.numOfReviews || 0,
+      }
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// recalculate avg rating right after review is saved
+ReviewSchema.post('save', async function () {
+  await this.constructor.calculateAverageRating(this.product)
+})
+
+// recalculate avg rating right after review is removed
+ReviewSchema.post('remove', async function () {
+  await this.constructor.calculateAverageRating(this.product)
+})
+
 export default mongoose.model('Review', ReviewSchema)
