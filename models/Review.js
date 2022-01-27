6f3dcf3ec52a8793c7dfc548dc396d1cb5232d53
Youngjae Lim <limyoungjae@gmail.com>
2022-01-26 19:35:26 -0500

    Added Review model

---
+++ models/Review.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose'
+
+const ReviewSchema = mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'Please provide rating'],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide review title'],
+      maxlength: 100,
+    },
+    comment: {
+      type: String,
+      required: [true, 'Please provide review text'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+// each user is allowed to leave a comment per product
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true })
+
+export default mongoose.model('Review', ReviewSchema)
